61da179c909d7e90fde87719f1f79ff73679a6c3
janine.derix <janine.derix@pixolith.de>
2015-04-24 12:48:17 +0200

    + new

--- site/snippets/fader.php
+++
@@ -1,9 +0,0 @@
-<section class="fader">
-    <div class="fader-wrapper">
-        <ul class="fader-container">
-            <li class="fade-item"></li>
-            <li class="fade-item"></li>
-            <div class="progress"></div>
-        </ul>
-    </div>
-</section>
--- site/templates/home.php
+++
@@ -1,8 +0,0 @@
-<?php snippet('header') ?>
-
-<main role="content">
-
-<h1>test test</h1>
-
-</main>
-<?php snippet('footer') ?>
